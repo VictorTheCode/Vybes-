f2ed41c0e9cf7a4c7c4354ea75127798da85df4e
Victor okpaire <vixtorjason@gmail.com>
2025-12-24 18:42:18 +0100

    refactor(db): remove test data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div>Merry Christmass</div>;
+const Home = async () => {
+  return (
+    <div className="min-h-screen bg-gray-80 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-(family-name:var(--font-geist-sans) text-[#333333]">
+        Vybes Blog
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
